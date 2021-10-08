fa21dbdedb8b0c5d28dbcefdba685fe2419c2f17
tejasvp25 <tejasvp25@gmail.com>
2021-10-08 06:44:01 +0000

    Adapt for Carbon

--- carbon_phoenix.mk
+++ carbon_phoenix.mk
@@ -6,19 +6,17 @@
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := carbon_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
-
-# ArrowOS Properties
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-#DEVICE_MAINTAINER := Sreekanth
 
 #PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
