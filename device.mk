087fc5ee35d3fa8ff67ea97855c3cd192c374484
tejasvp25 <tejasvp25@gmail.com>
2021-11-13 17:49:00 +0000

    Fix typo

--- device.mk
+++ device.mk
@@ -414,7 +414,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-carbon
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
