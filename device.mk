0cb6fa3352cb487913f60519b5291df276e646d3
kubersharma001 <kubersharma001@gmail.com>
2021-12-03 13:20:00 +0000

    phoenix: Compile libldacBT_bco.so
    08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- device.mk
+++ device.mk
@@ -150,7 +150,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libldacBT_bco
 
 # Camera
 PRODUCT_PACKAGES += \
